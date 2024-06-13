4367353cc554553fa651ec70ab5ed600d0309f89
Thomas Nakas <tnakas@1-E-4.42heilbronn.de>
2024-06-13 14:04:23 +0200

    adding fractol to the cursus

---
+++ 2nd_Cyrcle/Pipe_Ex/minitalk/libft/ft_atoi.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 12:16:13 by tnakas            #+#    #+#             */
+/*   Updated: 2024/04/26 01:52:18 by tnakas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	p_or_n(char c);
+int	is_space(char c);
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	neg;
+	int	atoi;
+
+	i = 0;
+	neg = 0;
+	atoi = 0;
+	while (is_space(*(str + i)))
+	{
+		i++;
+	}
+	if (p_or_n(*(str + i)))
+	{
+		if (*(str + i) == '-')
+			neg++;
+		i++;
+	}
+	if (p_or_n(*(str + i)))
+		return (0);
+	while (*(str + i) >= '0' && *(str + i) <= '9')
+	{
+		atoi = atoi * 10 + (*(str + i) - '0');
+		i++;
+	}
+	return (((neg % 2) != 0) * (-1) * atoi + ((neg % 2) == 0) * atoi);
+}
+
+int	p_or_n(char c)
+{
+	return ((c == '-' || c == '+') * 1);
+}
+
+int	is_space(char c)
+{
+	return ((((c >= 9) && (c <= 13)) || c == ' ') * 1);
+}
